668ee6d7fa2bd28578c78728f4960c350c561cae
HoanKool0205 <hoanlombard0205@gmail.com>
2024-10-18 16:56:54 +0700

    code SignUp SignIn

--- app/src/main/java/com/androidexp/englishapp/activity/LoginActivity.kt
+++ app/src/main/java/com/androidexp/englishapp/activity/LoginActivity.kt
@@ -1,21 +1,53 @@
 package com.androidexp.englishapp.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.androidexp.englishapp.MainActivity
 import com.androidexp.englishapp.R
+import com.androidexp.englishapp.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.tvDontAccountSignIn.setOnClickListener{
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
         }
+
+        binding.btnLogin.setOnClickListener {
+            val email = binding.edtEmail.text.toString()
+            val password = binding.edtPassword.text.toString()
+
+            if(email.isNotEmpty() && password.isNotEmpty()){
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener{
+                    if(it.isSuccessful){
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else{
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+            } else{
+                Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
--- app/src/main/java/com/androidexp/englishapp/activity/StartActivity.kt
+++ app/src/main/java/com/androidexp/englishapp/activity/StartActivity.kt
@@ -1,20 +1,29 @@
 package com.androidexp.englishapp.activity
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.androidexp.englishapp.R
+import com.androidexp.englishapp.databinding.ActivityStartBinding
 
 class StartActivity : AppCompatActivity() {
+    private val binding:ActivityStartBinding by lazy {
+        ActivityStartBinding.inflate(layoutInflater)
+    }
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
- //       val btnNext: Button = findViewById(R.id.btnNext)
-//        btnNext.setOnClickListener {
-//            Log.d("TAG", "onCreate: click")
-//            startActivity(Intent(this, ChooseLocationActivity::class.java))
-//            finish()
-//        }
+        val btnNext: Button = findViewById(R.id.btnNext)
+        btnNext.setOnClickListener {
+            Log.d("TAG", "onCreate: click")
+            startActivity(Intent(this,  LoginActivity::class.java))
+            finish()
+        }
     }
 }
